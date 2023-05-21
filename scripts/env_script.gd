a4942f227626798461fd6061be650ad180cfff3b
moaibad <fathulibad.412@gmail.com>
2023-05-21 23:03:10 +0700

    update code and fix poop

--- scripts/env_script.gd
+++ scripts/env_script.gd
@@ -6,7 +6,7 @@ signal player_entered
 
 
 func _ready():
-	timer.wait_time = 10
+	timer.wait_time = 15
 	timer.autostart = true
 # warning-ignore:return_value_discarded
 	timer.connect("timeout", self, "timer_timeout")
